2f7c590db6dc1ed4bca43b4b1cfea56074e843a9
Kalindu Balasuriya <61897592+KalinduBalasuriya@users.noreply.github.com>
2024-04-25 21:55:31 +0530

    Aded json error property into user login/ added current user route

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -4,12 +4,18 @@ const { User } = require("../models/user");
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const { signUp, login, getUsers } = require("../controllers/userController");
+const {
+  signUp,
+  login,
+  getUsers,
+  getCurrentUser,
+} = require("../controllers/userController");
 const authJwt = require("../Authentication/auth");
 const { validateRole } = require("../Authentication/accessController");
 
 router.post("/login", login);
 router.post("/signup", signUp);
 router.get("/allusers", authJwt(), validateRole(["Admin"]), getUsers);
+router.get("/currentuser", authJwt(), getCurrentUser);
 
 module.exports = router;
