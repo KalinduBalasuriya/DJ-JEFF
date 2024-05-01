93ecf535dbab7c639e20489fee8a45864e327a81
Kalindu Balasuriya <61897592+KalinduBalasuriya@users.noreply.github.com>
2024-05-01 12:31:31 +0530

    Added error handling for songs route. And updated the song model.

--- routers/songsRoutes.js
+++ routers/songsRoutes.js
@@ -4,6 +4,7 @@ const {
   addSong,
   getCategoryPlaylists,
   getPlaylist,
+  test,
 } = require("../controllers/songsController");
 const authJwt = require("../Authentication/auth");
 const { validateRole } = require("../Authentication/accessController");
@@ -15,6 +16,7 @@ router.get("/allsongs", getAllSongs);
 router.get("/allcategories", checkToken, getCategories);
 router.get("/categories/:categoryid", checkToken, getCategoryPlaylists);
 router.get("/categoryplaylists/:playlistid", checkToken, getPlaylist);
+router.post("/test", checkToken, test);
 
 router.post("/addsong", authJwt(), validateRole(["Admin"]), addSong);
 
